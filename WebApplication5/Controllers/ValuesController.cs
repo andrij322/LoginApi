41247de0181be111a54aacc38a6e5fb6387ea737
lil-dead <andrijuganov@gmail.com>
2019-07-02 14:14:12 +0300

    message

---
+++ WebApplication5/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.SessionState;
+using WebApplication5.Filters;
+
+namespace WebApplication5.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        [Route("{controler}/Login/{login}/{password}")]
+        public void Login([FromUri]string login,string password)
+        {
+            var session = System.Web.HttpContext.Current.Session;
+            session["Login"] = login;
+            session["Password"] = password;
+            if ((string)session["Login"] == "Login")
+                session["Logined"] = true;
+            else
+                session["Logined"] = false;
+
+        }
+
+        [AuthenticationFilter]
+        [HttpGet]
+        [Route("{controler}/act")]
+        public IHttpActionResult SomeAction()
+        {
+            return Content(HttpStatusCode.OK,"Some action");
+        }
+    }
+}
---
+++ WebApplication5/Filters/AuthenticationFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Http.Results;
+
+namespace WebApplication5.Filters
+{
+    public class AuthenticationFilter:Attribute,IAuthenticationFilter
+    {
+        public Task AuthenticateAsync(HttpAuthenticationContext context,CancellationToken token)
+        {
+            var logined = System.Web.HttpContext.Current.Session["Logined"];
+            if (logined==null||(bool)logined==false)
+                context.ErrorResult = new StatusCodeResult(HttpStatusCode.Unauthorized,context.Request);
+            return Task.FromResult<object>(null);
+        }
+        public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<object>(null);
+        }
+        public bool AllowMultiple
+        {
+            get { return false; }
+        }
+    }
+}
